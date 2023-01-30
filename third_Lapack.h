e598c09e4b82e3912628d96fdaf09522881615be
David Mayerich <mayerich@uh.edu>
2023-01-30 15:13:11 -0600

    fixed linux Eigen assertion bug

--- third_Lapack.h
+++ third_Lapack.h
@@ -74,7 +74,7 @@ Eigen::MatrixXcd MKL_multiply(Eigen::MatrixXcd& A, Eigen::MatrixXcd& B, std::com
 	C.resize(M, N);
 	cblas_zgemm(CblasColMajor, CblasNoTrans, CblasNoTrans, M, N, K, &alpha, (MKL_Complex16*)A.data(), M, (MKL_Complex16*)B.data(), K, &beta, (MKL_Complex16*)C.data(), M);
 
-	return A;
+	return C;
 }
 
 

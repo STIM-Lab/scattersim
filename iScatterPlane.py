23c6e890accd283280aab3ce2934bbf7aab4c6ec
David Mayerich <mayerich@uh.edu>
2023-04-28 17:06:40 -0500

    redesigned incoherent simulation script

--- iScatterPlane.py
+++ iScatterPlane.py
@@ -7,141 +7,101 @@ import time
 import math
 from tqdm import tqdm
 
-# Creating a Function.
-def normal_dist(x):
-    prob_density = (np.pi * np.std(x)) * np.exp(-0.5 * ((x - np.mean(x)) / np.std(x)) ** 2)
-    return prob_density
+def cart2sph(x, y, z):
+    azimuth = np.arctan2(y,x)
+    elevation = np.arctan2(z,np.sqrt(x**2 + y**2))
+    r = np.sqrt(x**2 + y**2 + z**2)
+    return azimuth, elevation, r
 
-# Return the intensity of the field everywhere.
+# Calculates the intensity of a complex X, Y, Z, 3 vector field
 def intensity(E):
-    Econj = np.conj(E)
+    Econj = np.conj(E)                                                          # calculate the complex conjugate
     I = np.sum(E * Econj, axis=-1)
     return np.real(I)
 
-def generate_points(diameter_xy, diameter_z, n_x, n_y, n_z, theta):
-    points = []
+# generate a set of points representing a rectangular focal spot
+# size is a tuple giving the span of the focal spot
+# samples is a tuple giving the number of points used to build the focal spot
+# theta is the rotation angle about the z-axis [0, 2pi]
+# phi is the rotation angle about the y-axis [0, pi/2]
+def generate_points(span, samples, theta, phi):
     rotate = True
-    radius = diameter_xy / 2
-    X = np.linspace(0, diameter_xy, n_x).reshape([1, 1, n_x])
-    Y = np.linspace(0, diameter_xy, n_y).reshape([1, n_y, 1])
-    Z = np.linspace(0, diameter_z, n_z).reshape([n_z, 1, 1])
-    # circle = (X - radius) ** 2 + (Y - radius) ** 2 <= radius ** 2
-    for j in range(len(Y[0])):
-        for i in range(len(X[0][0])):
-            for k in range(len(Z)):
-                # if circle[0][j][i] == True:
-                x_pos = (i - len(X[0][0]) // 2) * diameter_xy / len(X[0][0])
-                y_pos = (j - len(Y[0]) // 2) * diameter_xy / len(Y[0])
-                z_pos = (k - len(Z) // 2) * diameter_z / len(Z)
+    x = np.linspace(-span[0]/2, span[0]/2, samples[0])
+    y = np.linspace(-span[1]/2, span[1]/2, samples[1])
+    z = np.linspace(-span[2]/2, span[2]/2, samples[2])
+    
+    X, Y, Z = np.meshgrid(x, y, z)
+    
+    cos_phi = math.cos(phi)
+    sin_phi = math.sin(phi)
+    
+    Xn = cos_phi * X + sin_phi * Z
+    Yn = Y
+    Zn = -sin_phi * X + cos_phi * Z
 
-                if rotate == True:
-                    # https://en.wikipedia.org/wiki/Rotation_matrix#In_two_dimensions
-                    x_new_pos = math.cos(theta) * x_pos + math.sin(theta) * z_pos
-                    z_new_pos = -math.sin(theta) * x_pos + math.cos(theta) * z_pos
-                    points.append([x_new_pos, y_pos, z_new_pos])
-    print("How many points do we have? --" + str(len(points) * n_lambda))
-    return points
-def simulate_point(points, direction, wavelengths, output_directory, output_fig, n_CoherentWaves, size, resolution, alpha, obscuration):
-    # Simulate the fields and sum them up.
-    num = 0
-    # Run "scatterplane" n_lambda * len(points) * n_samples times. Generate len(points) ".npy" files
-    for w in range(len(wavelengths)):
-        Intensity = 0
-        for i in tqdm(range(len(points))):
-            in_str = output_directory + str(wavelengths[w]) + str(points[i][0]) + str(points[i][1]) + str(
-                points[i][2]) + ".cw"
-            out_str = output_directory + str(wavelengths[w]) + str(points[i][0]) + str(points[i][1]) + str(
-                points[i][2]) + ".npy"
+    return (np.ndarray.flatten(Xn), np.ndarray.flatten(Yn), np.ndarray.flatten(Zn))
 
-            proc = subprocess.run(["scatterplane", "--focus", str(points[i][0]), str(points[i][1]), str(points[i][2]),
-                                   "--nooutput", "--lambda", str(wavelengths[w]), "--output", in_str, "--alpha",
-                                   str(alpha),
-                                   "--beta", str(obscuration), "--direction", str(direction[0]), str(direction[1]),
-                                   str(direction[2]),
-                                   "--n", "1.0", "1.4", "--kappa", "0.005", "--samples", str(n_CoherentWaves), "--wavemask",
-                                   str(1), str(1), str(1)], shell=True, capture_output=True)
+# simulates a single focused field and saves the resulting cw file
+# filename - specify the name of the cw file that will be saved
+# position - 3D position of the focal point near the samples
+# direction - incoming illumination direction
+# wavelength - wavelength of the field
+# num_waves - number of samples used to simulate the field
+# alpha_aperture - outer aperture angle of the objective
+# beta_obscuration - angle of the center obscuration
+def simulate_point(filename, position, direction, wavelength, num_waves, alpha_aperture, beta_obscuration):
+    
+    
+    subprocess.run(["scatterplane", "--focus", str(position[0]), str(position[1]), str(position[2]),
+                        "--nooutput", "--lambda", str(wavelength), "--output", filename, "--alpha", str(alpha_aperture),
+                        "--beta", str(beta_obscuration), "--direction", str(direction[0]), str(direction[1]), str(direction[2]),
+                        "--n", "1.0", "1.4", "--kappa", "0.005", "--samples", str(num_waves)], shell=True, capture_output=True)
 
-            proc = subprocess.run(
-                ["scatterview", "--input", in_str, "--output", out_str, "--size", str(size), "--nogui", "--slice", "0",
-                 "--axis", "1",
+# run several simulations based on the number of points in the "points" array and save the corresponding cw files in the output directory
+# output_directory - location to store the output files
+# points - N x 3 array storing the positions of the focal points to be simulated
+# THE REST OF THE PARAMETERS MATCH simulate_point
+def simulate_points(output_directory, X, Y, Z, direction, wavelength, num_waves, alpha_aperture, beta_obscuration):
+    
+    for i in tqdm(range(len(X))):                                                     # for each point in the focal spot
+        filename = output_directory + str(i) + ".cw"
+        simulate_point(filename, (X[i], Y[i], Z[i]), direction, wavelength, num_waves, alpha_aperture, beta_obscuration)
+
+# convert cw files to sampled npy files representing the field and the desired points
+def sample_points(directory, span, resolution):
+    files = glob.glob(directory + "*.cw")
+    for fi in tqdm(range(len(files))):
+        subprocess.run(
+                ["scatterview", "--input", files[fi], "--output", files[fi] + ".npy", "--size", str(span), "--nogui", "--slice", "0", "--axis", "1",
                  "--resolution", str(resolution)], shell=True, capture_output=True)
+        
+    
+def sum_intensity(input_directory, span, resolution):
+    files = glob.glob(input_directory + "*.cw")
+    for fi in tqdm(range(len(files))):
+        E = np.load(files[fi] + ".npy")
+         
+        if fi == 0:
+            I = intensity(E)
+        else:
+            I = I + intensity(E)            
+    return I
 
-            num += 1
-            inFile = os.path.join(output_directory, out_str)
-            E = np.load(inFile)
-            if w == 0 and i == 0:
-                Intensity = abs(intensity(E))
-            else:
-                Intensity += abs(intensity(E))
-    print(num)
-    print("What is the number of the total waves? -- " + str(num))
-    # os.remove(inFile)
-    # os.remove(os.path.join(output_directory, in_str))
-    np.save(output_fig, Intensity)
-    return Intensity
+ 
+output_directory = "C:/Users/david/Desktop/penetration_tests/"
+wavelength = 0.3
+waves = 1800
+direction = [1, 0, 1]
+NA = 0.2
+resolution = 8
+span = 80
+theta, phi, r = cart2sph(direction[0], direction[1], direction[2])
 
+px, py, pz = generate_points((10, 10, 1), (12, 12, 2), theta, phi)
+#fig = plt.figure()
+#ax = fig.add_subplot(projection='3d')
+#ax.scatter(px, py, pz)
 
-start = time.time()
-# Clean the current folder
-output_directory = "D:\\myGit\\build\\scattersim\\tmp\\"
-if not os.listdir(output_directory):
-    print(" The root directory is clean.")
-else:
-    files = glob.glob(output_directory +"*")
-    for file in files:
-        os.remove(file)
-    print("The old files in the directory are deleted.")
-
-# Define background information for simulation
-direction = [0, 0, 1]  # Central direction
-# direction = [1, 0, 1/np.tan(math.radians(70))]
-wavelength_0 = 0.28     # The central wavelength
-n_lambda = 5           # The number of wavelengths
-wavelengths = np.linspace(wavelength_0-0.05, wavelength_0+0.05, n_lambda)
-
-# Define the optical system
-fname = "global"
-output_fig = output_directory + fname + ".npy"
-n_CoherentWaves = 200        # Number of coupled waves for each point
-size = 80               # The size of the canvas (um)
-resolution = 9
-alpha = 0.5              # Set the radian
-obscuration = alpha * 0.18    # Set percentage
-
-# Generate point sources
-diameter_xy = 20
-diameter_z = wavelength_0 * 2
-n_x = 10                # Sampling points along x
-n_y = 1                 # Sampling points along y
-n_z = 1                  # Sampling points along z
-theta = np.arctan(direction[0] / direction[2])
-points = generate_points(diameter_xy, diameter_z, n_x, n_y, n_z, theta)
-
-Intensity = simulate_point(points, direction, wavelengths, output_directory, output_fig, n_CoherentWaves, size, resolution, alpha, obscuration)
-
-# Visualization
-plt.imshow(Intensity, extent=(-size/2, size/2, size/2, -size/2))
-plt.set_cmap("afmhot")
-plt.colorbar(fraction=0.023, pad=0.3)
-# plt.title("Intensity")
-plt.show()
-
-# inten is part of Intensity. It is for extracting the uniform area and calculating the DOE
-inten = Intensity[len(Intensity)//2:len(Intensity)//4*3, len(Intensity)//4:len(Intensity)//4*3]
-plt.imshow(inten, extent=(-size/4, size/4, size//4, 0))
-plt.set_cmap("afmhot")
-plt.colorbar(fraction=0.023, pad=0.3)
-# plt.title("Intensity")
-plt.show()
-end = time.time()
-print("Total time is: " + str(end - start) + " s.")
-
-
-# Calculate the penetration depth
-target = 0
-threshold = np.mean(inten[target]) * 0.37
-for i in range(target+1, len(inten)):
-    if np.mean(inten[i]) < threshold:
-        depth = (i - target) / len(inten) * size/4
-        print("The penetration depth is: " + str(depth))
-        break
+simulate_points(output_directory, px, py, pz, direction, wavelength, waves, np.sin(NA), 0)
+sample_points(output_directory, span, resolution)
+I = sum_intensity(output_directory, span, resolution)

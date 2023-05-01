534b36ee00bd59e351504e31013a4dbb1d687eb0
David Mayerich <mayerich@uh.edu>
2023-05-01 10:46:11 -0500

    added alternative functions to define spots

--- iScatterPlane.py
+++ iScatterPlane.py
@@ -8,8 +8,8 @@ import math
 from tqdm import tqdm
 
 def cart2sph(x, y, z):
-    azimuth = np.arctan2(y,x)
-    elevation = np.arctan2(z,np.sqrt(x**2 + y**2))
+    azimuth = np.arctan2(y, x)
+    elevation = np.arctan2(z, np.sqrt(x**2 + y**2))
     r = np.sqrt(x**2 + y**2 + z**2)
     return azimuth, elevation, r
 
@@ -20,16 +20,17 @@ def intensity(E):
     return np.real(I)
 
 # generate a set of points representing a rectangular focal spot
-# size is a tuple giving the span of the focal spot
+# spotsize is a tuple giving the size of the focal spot
 # samples is a tuple giving the number of points used to build the focal spot
 # theta is the rotation angle about the z-axis [0, 2pi]
 # phi is the rotation angle about the y-axis [0, pi/2]
-def generate_points(span, samples, phi):
+def generate_points_rect(spotsize, samples, phi):
     rotate = True
-    x = np.linspace(-span[0]/2, span[0]/2, samples[0])
-    y = np.linspace(-span[1]/2, span[1]/2, samples[1])
-    z = np.linspace(-span[2]/2, span[2]/2, samples[2])
+    x = np.linspace(-spotsize[0]/2, spotsize[0]/2, samples[0])
+    y = np.linspace(-spotsize[1]/2, spotsize[1]/2, samples[1])
+    z = np.linspace(-spotsize[2]/2, spotsize[2]/2, samples[2])
     
+    # calculate the coordinates of the points before rotation
     X, Y, Z = np.meshgrid(x, y, z)
     
     cos_phi = math.cos(phi)
@@ -38,6 +39,61 @@ def generate_points(span, samples, phi):
     Xn = cos_phi * X + sin_phi * Z
     Yn = Y
     Zn = -sin_phi * X + cos_phi * Z
+
+    return (np.ndarray.flatten(Xn), np.ndarray.flatten(Yn), np.ndarray.flatten(Zn))
+
+# generate a set of points representing a rectangular focal spot
+# spotsize is a tuple giving the diameter of the focal spot
+# samples is a tuple giving the number of points used to build the focal spot
+# theta is the rotation angle about the z-axis [0, 2pi]
+# phi is the rotation angle about the y-axis [0, pi/2]
+def generate_points_circ(spotsize, thickness, samples, phi):
+    
+    radius = spotsize/2.0
+    
+    r = radius * np.sqrt(np.random.random(samples))
+    theta = np.random.random(samples) * 2 * np.pi
+    
+    x = r * np.cos(theta)
+    y = r * np.sin(theta)
+    z = np.random.random(samples) * thickness - thickness/2
+    
+    cos_phi = math.cos(phi)
+    sin_phi = math.sin(phi)
+    
+    xn = cos_phi * x + sin_phi * z
+    yn = y
+    zn = -sin_phi * x + cos_phi * z
+    
+
+    return (xn, yn, zn)
+
+# generate a set of points representing a rectangular focal spot
+# spotsize is a tuple giving the diameter of the focal spot
+# samples is a tuple giving the number of points used to build the focal spot
+# theta is the rotation angle about the z-axis [0, 2pi]
+# phi is the rotation angle about the y-axis [0, pi/2]
+def generate_points_uniform(spotsize, thickness, samples, phi):
+    
+    radius = spotsize/2.0
+    
+    r = np.linspace(0, 1, samples[0])
+    d_theta = 2 * np.pi / samples[0]
+    theta = np.linspace(0, d_theta * (samples[0] - 1), samples[1])
+    z = np.linspace(-thickness/2, thickness/2, samples[2])
+    
+    R, THETA, Z = np.meshgrid(radius * np.sqrt(r), theta, z)
+        
+    X = R * np.cos(THETA)
+    Y = R * np.sin(THETA)
+    
+    cos_phi = math.cos(phi)
+    sin_phi = math.sin(phi)
+    
+    Xn = cos_phi * X + sin_phi * Z
+    Yn = Y
+    Zn = -sin_phi * X + cos_phi * Z
+    
 
     return (np.ndarray.flatten(Xn), np.ndarray.flatten(Yn), np.ndarray.flatten(Zn))
 
@@ -68,14 +124,15 @@ def simulate_points(output_directory, X, Y, Z, direction, wavelength, num_waves,
         simulate_point(filename, (X[i], Y[i], Z[i]), direction, wavelength, num_waves, alpha_aperture, beta_obscuration)
 
 # convert cw files to sampled npy files representing the field and the desired points
-def sample_points(directory, span, resolution):
+def sample_points(directory, simulation_range, resolution, axis=1):
     files = glob.glob(directory + "*.cw")
     for fi in tqdm(range(len(files))):
         subprocess.run(
-                ["scatterview", "--input", files[fi], "--output", files[fi] + ".npy", "--size", str(span), "--nogui", "--slice", "0", "--axis", "1",
+                ["scatterview", "--input", files[fi], "--output", files[fi] + ".npy", "--size", str(simulation_range), "--nogui", "--slice", "0", "--axis", str(axis),
                  "--resolution", str(resolution)], shell=True, capture_output=True)
+        
 
-def sum_intensity(input_directory, span, resolution):
+def sum_intensity(input_directory, resolution):
     files = glob.glob(input_directory + "*.cw")
     I = 0
     for fi in tqdm(range(len(files))):
@@ -88,8 +145,8 @@ def sum_intensity(input_directory, span, resolution):
 
 
 
-# output_directory = "C:/Users/david/Desktop/penetration_tests/"
-output_directory = "D:\\myGit\\build\\scattersim\\tmp\\"
+output_directory = "C:/Users/david/Desktop/penetration_tests/"
+#output_directory = "D:\\myGit\\build\\scattersim\\tmp\\"
 # Clean the folder
 if not os.listdir(output_directory):
     print(" The root directory is clean.")
@@ -100,22 +157,31 @@ else:
     print("The old files in the directory are deleted.")
 wavelength = 0.28
 waves = 1000
-direction = [1, 0, 1]
+points = (20, 20, 1)
+total_pts = points[0] * points[1] * points[2]
+direction = [0, 0, 1]
 NA = 0.2
+NAo = 0
 resolution = 8
-span = 80
+spotsize = (10, 10, 1)
 theta, phi, r = cart2sph(direction[0], direction[1], direction[2])
+# adjust the elevation so that it corresponds to radians *from* the z axis
+phi = np.pi/2.0 - phi
+axis = 2
 
-px, py, pz = generate_points((20, 0, 1), (60, 1, 2), phi)
+#px, py, pz = generate_points_rect(spotsize, points, phi)
+#px, py, pz = generate_points_circ(spotsize[0], spotsize[2], total_pts, phi)
+px, py, pz = generate_points_uniform(spotsize[0], spotsize[2], points, phi)
+
 #fig = plt.figure()
 #ax = fig.add_subplot(projection='3d')
 #ax.scatter(px, py, pz)
 
-simulate_points(output_directory, px, py, pz, direction, wavelength, waves, np.sin(NA), 0)
-sample_points(output_directory, span, resolution)
-I = sum_intensity(output_directory, span, resolution)
+simulate_points(output_directory, px, py, pz, direction, wavelength, waves, np.arcsin(NA), np.arcsin(NAo))
+sample_points(output_directory, spotsize[0] * 2, resolution, axis)
+I = sum_intensity(output_directory, resolution)
 
-plt.imshow(I, extent=(-span/2, span/2, span/2, -span/2))
+plt.imshow(I, extent=(-spotsize[0], spotsize[0], spotsize[0], -spotsize[0]))
 plt.set_cmap("afmhot")
 plt.colorbar()
 # plt.title("Intensity")

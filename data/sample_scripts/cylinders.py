557532024ec8510000d1338b76c929d5294d705c
David Mayerich <mayerich@uh.edu>
2023-10-26 11:50:28 -0500

    added a sample scripts directory and cylinder script

---
+++ data/sample_scripts/cylinders.py
@@ -0,0 +1,28 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# size of the sample
+S = 10
+
+# number of samples and layers
+N = 100
+L = N
+
+# radius of the cylinder
+a = 2
+
+# refractive index of the cylinder
+n = 1.4 + 0.05j
+
+x = np.linspace(-S/2.0, S/2.0, N)
+X, Z = np.meshgrid(x, x)
+
+R = np.sqrt(X**2 + Z**2)
+
+C = np.ones_like(R, dtype=np.complex128)
+C[R <= a] = n
+C = np.expand_dims(C, 1)
+
+np.save("../cylinder.npy", C)
+
+
